f04c5b483ed5106fa28ac5b1ed2e396c270c6c47
Kwame Twum <>
2022-12-12 09:04:04 +0000

    Added cover_art_type implementation

--- server/services/card/CardService.js
+++ server/services/card/CardService.js
@@ -29,20 +29,17 @@ class CardService {
         const data = {id, ...rq.body, ...{owner_id: user.id}}
 
         if (data.media && data.media_demo) {
-            console.log('1 down')
             const file_name = `media/raw/${id}.mp3`
             const buffer = Buffer.from(data.media.replace(/^data:audio\/\w+;base64,/, ""), 'base64')
             await UploadService.uploadFileInB64(buffer, file_name, 'audio/mpeg')
             data.media_url = `${CONSTANTS.S3}${file_name}`
 
-            console.log('2 down')
             const demo_file_name = `media/crop/${id}.mp3`
             const buffer2 = Buffer.from(data.media_demo, 'base64')
             await UploadService.uploadFileInB64(buffer2, demo_file_name, 'audio/mpeg')
             data.media_demo_url = `${CONSTANTS.S3}${demo_file_name}`
 
-            console.log('3 down')
-            const x = `media/cover/${id}.jpg`
+            const x = `media/cover/${id}.${data.cover_art_type}`
             const bufferx = Buffer.from(data.cover_art.replace(/^data:image\/\w+;base64,/, ""), 'base64')
             await UploadService.uploadFileInB64(bufferx, x, 'image/jpeg')
             data.cover_art_url = `${CONSTANTS.S3}${x}`
@@ -83,7 +80,7 @@ class CardService {
                 data.media_url = `${CONSTANTS.S3}${fileName}`
             }
             if (_.has(rq.files, 'cover_art')) {
-                const fileName = `media/cover/${id}.jpg`
+                const fileName = `media/cover/${id}.${data.cover_art_type}`
                 await UploadService.uploadFile(rq.files.cover_art[0], fileName)
                 data.cover_art_url = `${CONSTANTS.S3}${fileName}`
             }
